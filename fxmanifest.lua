28c946ccb3dddfc4849b400b08be1b372aa3f7b1
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-03 21:01:31 -0400

    updated fxmanifest to use newest version, added support for rdr3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,17 @@
-fx_version 'bodacious'
-game 'gta5'
+fx_version 'adamant'
+games {'rdr3'}
 
-name 'FiveM Typescript Boilerplate'
-description 'A boilerplate for using Typescript in FiveM'
-author 'Remco Troost (d0p3t)'
-url 'https://github.com/d0p3t/fivem-ts-boilerplate'
+name 'TypeScript Boilerplate'
+description 'An updated TypeScript boilerplate for RedM'
+author 'Ryan "Standal" Lockard'
 
-client_script 'dist/client/*.client.js'
-server_script 'dist/server/*.server.js'
+client_scripts {
+  'dist/client/*.client.js',
+}
+server_script {
+  'dist/server/*.server.js',
+  -- '@mysql-async/lib/MySQL.lua',
+  -- ...
+}
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'

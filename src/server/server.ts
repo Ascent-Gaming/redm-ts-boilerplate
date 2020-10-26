223070cfdfa028c2872e4519f0c06efc0e709c65
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-26 18:48:32 -0400

    added shared "./src/util/" directory, removed boilerplate code

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,1 @@
-onNet('helloserver', () => {
-  const _source = (global as any).source;
-
-  console.log(`Hello from ${_source}`);
-
-  emitNet('helloclient', _source, 'i got your message!');
-});
+import { Delay } from "../util"

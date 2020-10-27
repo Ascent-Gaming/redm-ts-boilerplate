7d263ac2dae26e70ef8ea72f95af240ec0f1b7f6
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2020-10-26 21:05:57 -0400

    added CfxLog function

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,21 @@
+import chalk from "chalk"
+import * as Config from "../../config.json"
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 export const Delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export type CfxLogParams = {
+  level: LogLevel
+  resource?: string
+  output: string
+}
+export const CfxLog = (params: CfxLogParams) => {
+  if (params.level < Config.resource.logLevel) {
+    console.log(
+      chalk.redBright(chalk.bgBlackBright(
+        chalk.bold(params.resource || Config.resource.name),
+        params.output
+      ))
+    )
+  }
+}
